7654c76dba661e4d347569d1deaaedab7b3cea78
fido1hn <confidoekeanya@gmail.com>
2023-12-29 21:42:42 +0100

    Add success message & toast after profile update

--- core/customer/views.py
+++ core/customer/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from . import forms
 
@@ -18,10 +19,14 @@ def profile_page(request):
         user_form = forms.BasicUserForm(request.POST, instance=request.user)
         customer_form = forms.BasicCustomerForm(
             request.POST, request.FILES, instance=request.user.customer)
+
         if user_form.is_valid() and customer_form.is_valid():
             user_form.save()
             customer_form.save()
+
+            messages.success(request, "Profile updated successfully.")
             return redirect("customer:profile")
+
     return render(request, "core/customer/profile.html", {
         "user_form": user_form,
         "customer_form": customer_form,
--- tastyexpress/settings.py
+++ tastyexpress/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib import messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -156,3 +157,11 @@ AUTHEHTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
 )
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
