6c2eeaf645066d7c8c983cb5e60da1872b6a606b
fido1hn <confidoekeanya@gmail.com>
2023-12-29 11:59:34 +0100

    Add customer profile page and form

--- core/customer/views.py
+++ core/customer/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
+from . import forms
+
 
 @login_required()
 def home(request):
-    return render(request, "core/customer/base.html")
+    return redirect("customer:profile")
+
+
+@login_required(login_url="/sign-in/?next=/customer/")
+def profile_page(request):
+    user_form = forms.BasicUserForm(instance=request.user)
+    if request.method == "POST":
+        user_form = forms.BasicUserForm(request.POST, instance=request.user)
+        if user_form.is_valid():
+            user_form.save()
+            return redirect("customer:profile")
+    return render(request, "core/customer/profile.html", {
+        "user_form": user_form,
+    })
--- core/urls.py
+++ core/urls.py
@@ -2,11 +2,12 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_view
 
 from . import views
-from core.customer import views as customer_views
-from core.courier import views as courier_views
+from .customer import views as customer_views
+from .courier import views as courier_views
 
 customer_urlpatterns = [
     path('', customer_views.home, name='home'),
+    path('profile/', customer_views.profile_page, name='profile'),
 ]
 courier_urlpatterns = [
     path('', courier_views.home, name='home'),
