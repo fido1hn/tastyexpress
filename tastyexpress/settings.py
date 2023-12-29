6db30e739ec9a0b2d4bc381e8ac423efa7883024
fido1hn <confidoekeanya@gmail.com>
2023-12-29 14:48:32 +0100

    Add Customer model and ProfileMiddleware

--- tastyexpress/settings.py
+++ tastyexpress/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
+    'core.middleware.ProfileMiddleware',
 ]
 
 ROOT_URLCONF = 'tastyexpress.urls'
