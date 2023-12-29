591e7634286c778b30c45bb2969f59216575ae56
fido1hn <confidoekeanya@gmail.com>
2023-12-29 23:27:13 +0100

    Add password change functionality to customer profile

--- core/customer/views.py
+++ core/customer/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
 
 from . import forms
 
@@ -14,20 +16,33 @@ def home(request):
 def profile_page(request):
     user_form = forms.BasicUserForm(instance=request.user)
     customer_form = forms.BasicCustomerForm(instance=request.user.customer)
+    password_form = PasswordChangeForm(request.user)
 
     if request.method == "POST":
-        user_form = forms.BasicUserForm(request.POST, instance=request.user)
-        customer_form = forms.BasicCustomerForm(
-            request.POST, request.FILES, instance=request.user.customer)
 
-        if user_form.is_valid() and customer_form.is_valid():
-            user_form.save()
-            customer_form.save()
+        if request.POST.get("action") == "update_profile":
+            user_form = forms.BasicUserForm(
+                request.POST, instance=request.user)
+            customer_form = forms.BasicCustomerForm(
+                request.POST, request.FILES, instance=request.user.customer)
 
-            messages.success(request, "Profile updated successfully.")
-            return redirect("customer:profile")
+            if user_form.is_valid() and customer_form.is_valid():
+                user_form.save()
+                customer_form.save()
+                messages.success(request, "Your profile updated successfully.")
+                return redirect("customer:profile")
+
+        elif request.POST.get("action") == "update_password":
+            password_form = PasswordChangeForm(request.user, request.POST)
+            if password_form.is_valid():
+                user = password_form.save()
+                update_session_auth_hash(request, user)
+                messages.success(
+                    request, "Your password updated successfully.")
+                return redirect("customer:profile")
 
     return render(request, "core/customer/profile.html", {
         "user_form": user_form,
         "customer_form": customer_form,
+        "password_form": password_form,
     })
